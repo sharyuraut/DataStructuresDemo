4d811e1e2bfd5faadff8f2b8e66d943f0e923a4f
sharyu raut <sharyukraut210298@gmail.com>
2020-10-21 09:34:10 +0530

    [Sharyu] Add. uc1 linklist created

---
+++ DataStructuresDemo/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresDemo
+{
+    class LinkedList
+    {
+        internal Node head;
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " inserted into linked list");
+        }
+        internal void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList Empty!");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructuresDemo/Program.cs
+++ DataStructuresDemo/Program.cs
@@ -7,6 +7,13 @@ namespace DataStructuresDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Linked List");
+            LinkedList list = new LinkedList();
+            list.Add(11);
+            list.Add(08);
+            list.Add(97);
+            list.Display();
         }
     }
 }
